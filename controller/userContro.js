bd6b5b752acaaabd61a3a2afb1498b17d9f0d335
ArunkumarVK <vkarunkumar446@gmail.com>
2023-08-01 15:30:20 +0530

    backend code updated

--- controller/userContro.js
+++ controller/userContro.js
@@ -102,3 +102,58 @@ exports.InsertEmployee = async (req,res)=>{
     })})
   }
 }
+
+//Get User
+exports.GetUser = (req,res)=>{
+  jwt.verify(req.token, process.env.SECRET_KEY,async function(err, decoded) {
+    // console.log(req.header("authorization"));
+    if (err) {
+      res.status(400).json({
+        message: "Invalid Token",
+      }) 
+    }else{
+      const validUser = await Employee.findOne({email:decoded.user.email})
+      // console.log(validUser);
+      if(validUser?.role_id==1){
+        await Employee.findOne({email:validUser.email}).then(data=>{
+          res.status(200).json({
+            role: validUser.role,
+            User: data
+          })
+        }).catch(err=>{
+          res.status(400).json({
+            error: err
+          })
+        })
+      }else if(validUser?.role_id==4){
+        await Employee.find().then(data=>{
+          let users = data.filter(i=>i.role_id==1 || i.role_id==2)
+          res.status(200).json({
+            Role: validUser.role,
+            Users : users
+          })
+        }).catch (err=>{
+          res.status(400).json({
+            error: err
+          })
+        })
+
+      }else if(validUser?.role_id==2){
+        {
+          await Employee.find().then(user=>{
+           let users = user.filter(i=> i.role_id==1)
+           res.status(200).json({
+            User: validUser.role,
+            Users: users
+           })
+          })
+        }
+
+      }else{
+        res.status(200).json({
+          message: "Employee Details not Updated Please Contact Admin"
+        })
+      }
+    }
+  });
+}
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const router = express.Router();
-const userController = require("../controller/userContro")
+const userController = require("../controller/userContro");
+const {verifyToken} = require("../verifyToken")
 
-const {register, Login, InsertEmployee} = userController
+const {register, Login, InsertEmployee, GetUser} = userController
 
 router.route("/register").post(register)
 router.route("/login").post(Login)
-router.route("/insert").post(InsertEmployee)
+router.route("/insert").post(InsertEmployee);
+router.post("/getUser",verifyToken, GetUser)
 
 module.exports = router; 
